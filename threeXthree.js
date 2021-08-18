440500b151ef39014e05f58048e0a2e1dd08ed8d
cburull <88709887+cburull@users.noreply.github.com>
2021-08-18 12:23:54 +0200

    Delete threeXthree.js

--- threeXthree.js
+++
@@ -1,9 +0,0 @@
-function hover(box){
-    box.style.border ="grey 1px solid"
-}
-function noHover(box){
-    box.style.border ="none"
-}
-function klikk(box){
-    box.style.backgroundColor ="rgb(100,100,100)"
-}
